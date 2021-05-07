53eec82b90048ef0c24485f0d2238d3cc21bc451
Binod Sahu <sahubinod85@gmail.com>
2021-05-07 01:47:26 +0100

    suffle and sort finctionality addedadded

--- js/sort.js
+++ js/sort.js
@@ -1,7 +1,32 @@
-const suffleNode = () => {
-    console.log('suffleNode')
-}
+const getShuffledArr = a => {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+};
 
+const suffleNode = () => {
+    const list = document.getElementById("nodeList");
+    let nodes = list.children;
+    let i = 0;
+    nodes = Array.prototype.slice.call(nodes);
+    nodes = getShuffledArr(nodes);
+    while(i < nodes.length)
+    {
+        list.appendChild(nodes[i]);
+        ++i;
+    }
+} 
 const sortNode = () => {
-    console.log('sort node')
+    const list = document.getElementById("nodeList");
+    let nodes = list.children;
+    let i = 0;
+    nodes = Array.prototype.slice.call(nodes);
+    nodes = nodes.sort(function(a, b){return a.innerText-b.innerText});;
+    while(i < nodes.length)
+    {
+        list.appendChild(nodes[i]);
+        ++i;
+    }
 }
